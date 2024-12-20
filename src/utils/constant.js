52966e4a2597460d32bb0252f449e4ec51f53b14
KTasdemir33 <128632059+KTasdemir33@users.noreply.github.com>
2024-12-20 20:58:03 +0300

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,7 +4,7 @@ class AppConstant {
   static maxGas = 500000;
   static ritsuswapETHtoUSDC = "0x7160570BB153Edd0Ea1775EC2b2Ac9b65F1aB61B";
   static astraboost = "0x90CE48ED68C6FCAe6F13b445F1573f003cF1804d";
-  static minGasPrice = 0.05 * 1e9; // in wei
-  static maxGasPrice = 0.06 * 1e9; // in wei
+  static minGasPrice = 0.12 * 1e9; // in wei
+  static maxGasPrice = 0.17 * 1e9; // in wei
 }
 module.exports = AppConstant;
