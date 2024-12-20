a6c6cb674b70dcf84fefe981a160adf73fa2418c
KTasdemir33 <128632059+KTasdemir33@users.noreply.github.com>
2024-12-21 01:08:23 +0300

    Update wrap.js

--- src/module/wrap/wrap.js
+++ src/module/wrap/wrap.js
@@ -1,39 +1,52 @@
-const { web3, walletAddress, privateKey } = require('../../../config/web3');
+const { web3, walletAddress, switchRpc } = require('../../../config/web3');
 const AppConstant = require('../../utils/constant');
+require('dotenv').config();
 
-const wrapABI = [
+const unwrapABI = [
     {
         "constant": false,
-        "inputs": [],
-        "name": "deposit",
+        "inputs": [
+            {
+                "name": "wad",
+                "type": "uint256"
+            }
+        ],
+        "name": "withdraw",
         "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
+        "payable": false,
+        "stateMutability": "nonpayable",
         "type": "function"
     }
 ];
 
-const wrapContract = new web3.eth.Contract(wrapABI, AppConstant.wrap);
+const contract = new web3.eth.Contract(unwrapABI, AppConstant.wrap);
 
-async function wrap(amount, gasPrice) {
-    const nonce = await web3.eth.getTransactionCount(walletAddress);
+async function unwrap(amount, gasPrice) {
+    let nonce;
+    try {
+        nonce = await web3.eth.getTransactionCount(walletAddress);
+    } catch (error) {
+        console.error(`Error getting nonce: ${error.message}`);
+        web3 = switchRpc();
+        nonce = await web3.eth.getTransactionCount(walletAddress);
+    }
+
     const tx = {
         from: walletAddress,
         to: AppConstant.wrap,
-        value: web3.utils.toWei(amount.toString(), 'ether'),
         gas: AppConstant.maxGas,
         gasPrice: gasPrice,
-        data: wrapContract.methods.deposit().encodeABI(),
+        data: contract.methods.withdraw(web3.utils.toWei(amount.toString(), 'ether')).encodeABI(),
         nonce: nonce,
         chainId: 167000
     };
 
-    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+    const signedTx = await web3.eth.accounts.signTransaction(tx, process.env.PRIVATE_KEY);
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 
     return receipt.transactionHash;
 }
 
 module.exports = {
-    wrap
+    unwrap
 };
