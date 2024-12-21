969a712a0411451aa33bda1da21ada77999d0e6c
KTasdemir33 <128632059+KTasdemir33@users.noreply.github.com>
2024-12-21 23:37:37 +0300

    Create constant.js

---
+++ src/module/wrap/constant.js
@@ -0,0 +1,12 @@
+class AppConstant {
+  static wrap = "0xA51894664A773981C6C112C43ce576f315d5b1B6";
+  static API = "6KCICCIVER24EKE5DNQ8HQDN5BAQSD19UD";
+  static maxGas = 500000;
+  static minterest = ""; // Artık kullanılmadığı için boş bırakılabilir
+  static ritsuswapETHtoUSDC = "0x7160570BB153Edd0Ea1775EC2b2Ac9b65F1aB61B";
+  static astraboost = "";
+  static tax = ""; // Artık kullanılmadığı için boş bırakılabilir
+  static minGasPrice = 0.05 * 1e9; // in wei
+  static maxGasPrice = 0.06 * 1e9; // in wei
+}
+module.exports = AppConstant;
