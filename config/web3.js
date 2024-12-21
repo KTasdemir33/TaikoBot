728f92205be28cee07e81d9c2e2252a3b597af37
KTasdemir33 <128632059+KTasdemir33@users.noreply.github.com>
2024-12-21 18:14:52 +0300

    Update web3.js

--- config/web3.js
+++ config/web3.js
@@ -1,7 +1,7 @@
-const Web3 = require('web3');
 require('dotenv').config();
+const Web3 = require('web3');
 
-const endpoints = [
+const rpcUrls = [
     'https://rpc.taiko.xyz',
     'https://rpc.mainnet.taiko.xyz',
     'https://rpc.ankr.com/taiko',
@@ -9,19 +9,30 @@ const endpoints = [
     'https://taiko.blockpi.network/v1/rpc/public'
 ];
 
-let currentEndpointIndex = 0;
-let web3 = new Web3(endpoints[currentEndpointIndex]);
+let currentRpcIndex = 0;
 
-const switchRpc = () => {
-    currentEndpointIndex = (currentEndpointIndex + 1) % endpoints.length;
-    web3 = new Web3(endpoints[currentEndpointIndex]);
+function getWeb3() {
+    const web3 = new Web3(new Web3.providers.HttpProvider(rpcUrls[currentRpcIndex]));
     return web3;
-};
+}
 
-const walletAddress = web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY).address;
+function switchRpc() {
+    currentRpcIndex = (currentRpcIndex + 1) % rpcUrls.length;
+    console.log(`Switching to RPC: ${rpcUrls[currentRpcIndex]}`);
+    return getWeb3();
+}
+
+const web3 = getWeb3();
+const privateKey = process.env.PRIVATE_KEY;
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+const walletAddress = account.address;
+
+console.log("Wallet Address:", walletAddress);
 
 module.exports = {
+    getWeb3,
     web3,
     walletAddress,
+    privateKey,
     switchRpc
 };
