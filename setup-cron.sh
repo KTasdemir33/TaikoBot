c9499f1702e0fb4d5d42de8d7187fcf850c38b58
KTasdemir33 <128632059+KTasdemir33@users.noreply.github.com>
2024-12-21 18:26:18 +0300

    Update setup-cron.sh

--- setup-cron.sh
+++ setup-cron.sh
@@ -16,10 +16,21 @@ if [ ! -f "$PROJECT_PATH/index.js" ]; then
   exit 1
 fi
 
-# Cron işini tanımlama
-CRON_JOB="30 8 * * * $NODE_PATH $PROJECT_PATH/index.js"
+# Rastgele bir saat ve dakika belirleme (08:00 - 09:30 arası)
+HOUR=$((8 + RANDOM % 2)) # 08 veya 09
+MINUTE=$((RANDOM % 60)) # 00-59
 
-# Cron işinin zaten var olup olmadığını kontrol etme ve ekleme
-(crontab -l 2>/dev/null | grep -Fxq "$CRON_JOB") || (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+if [ "$HOUR" -eq 9 ] && [ "$MINUTE" -gt 30 ]; then
+  MINUTE=$((RANDOM % 31)) # 09:00-09:30
+fi
 
-echo "Cron job added successfully. The bot will run every day at 08:30 UTC."
+# Cron job'u tanımlama
+CRON_JOB="$MINUTE $HOUR * * * $NODE_PATH $PROJECT_PATH/index.js"
+
+# Önceki cron job'u kaldırma
+(crontab -l | grep -v "$NODE_PATH $PROJECT_PATH/index.js") | crontab -
+
+# Yeni cron job'u ekleme
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+
+echo "Cron job added successfully. The bot will run every day at a random time between 08:00 and 09:30 UTC."
