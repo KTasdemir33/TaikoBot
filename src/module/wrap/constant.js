46be296c75891ac6c7193c9bda386e5ff0fa29b4
KTasdemir33 <128632059+KTasdemir33@users.noreply.github.com>
2024-12-22 00:21:07 +0300

    Update constant.js

--- src/module/wrap/constant.js
+++ src/module/wrap/constant.js
@@ -1,4 +1,5 @@
 class AppConstant {
   static wrap = "0xA51894664A773981C6C112C43ce576f315d5b1B6";
+  static ritsuswapETHtoUSDC = "0x7160570BB153Edd0Ea1775EC2b2Ac9b65F1aB61B";
 }
 module.exports = AppConstant;
